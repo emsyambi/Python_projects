bd05730367d75b21d2e045c4c583f362dfae27f0
Emmanuel Syambi <61493767+emsyambi@users.noreply.github.com>
2021-07-14 11:24:49 +0300

    Add files via upload

---
+++ prime_numbers.py
@@ -0,0 +1,10 @@
+# the code below finds prime numbers in a given range
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
